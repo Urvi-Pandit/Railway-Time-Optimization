d2f9f2293044331afec3b9bfda820c3099a8632e
Urvi Pandit <2020.urvi.pandit@ves.ac.in>
2022-09-16 18:56:11 +0530

    Check files now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <label className="bg-white">Hi</label>
+      {/* <div className="w-full max-w-xs">
+        <form className="form" className="shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-yellow-50 opacity-0">
+          <div className="inner">
+              <label className="block text-gray-700 text-sm font-bold mb-2">Enter Source</label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+          </div>
+          <div>    
+              <label className="block text-gray-700 text-sm font-bold mb-2">Enter Destination</label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"  />
+          </div>
+          <div>    
+              <label className="block text-gray-700 text-sm font-bold mb-2">Select Time</label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"  type="time" />
+          </div>    
+        </form>
+      </div>
+      <div className="">
+        <h4>Welcome to RailSave!</h4>
+        <b>Here is our humble attempt to help you with optimizing your travel time over the Local</b>
+      </div> */}
     </div>
   );
 }
